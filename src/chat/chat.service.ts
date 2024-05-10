c12b8d1593ca77b32c48d761fa48f7f77ea660f3
iambasantarai <iambasantarai@gmail.com>
2024-05-10 21:00:46 +0545

    Create an agent to perform web search

--- src/chat/chat.service.ts
+++ src/chat/chat.service.ts
@@ -1,12 +1,47 @@
 import { Injectable } from '@nestjs/common';
 import { CreateChatDto } from './dto/create-chat.dto';
+import { ChatOpenAI } from '@langchain/openai';
+import { TavilySearchResults } from '@langchain/community/tools/tavily_search';
+import { createToolCallingAgent } from 'langchain/agents';
+import { ChatPromptTemplate } from '@langchain/core/prompts';
+import { AgentExecutor } from 'langchain/agents';
 
 @Injectable()
 export class ChatService {
-  create(createChatDto: CreateChatDto) {
+  async create(createChatDto: CreateChatDto) {
+    const { query } = createChatDto;
+
+    const llm = new ChatOpenAI({
+      model: 'gpt-3.5-turbo-0125',
+      temperature: 0,
+    });
+
+    const tools = [new TavilySearchResults({ maxResults: 1 })];
+
+    const prompt = ChatPromptTemplate.fromMessages([
+      ['system', 'You are a helpful assistant'],
+      ['placeholder', '{chat_history}'],
+      ['human', '{input}'],
+      ['placeholder', '{agent_scratchpad}'],
+    ]);
+
+    const agent = await createToolCallingAgent({
+      llm,
+      tools,
+      prompt,
+    });
+
+    const agentExecutor = new AgentExecutor({
+      agent,
+      tools,
+    });
+
+    const result = await agentExecutor.invoke({
+      input: query,
+    });
+
     return {
-      query: createChatDto.query,
-      answer: 'Hi, How may i help you?',
+      result,
     };
   }
 }
