a07307d0c295ad70404ca6820c1f35e632cabb8b
iambasantarai <iambasantarai@gmail.com>
2024-05-10 23:31:31 +0545

    Create an agent that performs google search

--- src/chat/chat.service.ts
+++ src/chat/chat.service.ts
@@ -1,32 +1,46 @@
 import { Injectable } from '@nestjs/common';
 import { CreateChatDto } from './dto/create-chat.dto';
 import { ChatOpenAI } from '@langchain/openai';
-import { TavilySearchResults } from '@langchain/community/tools/tavily_search';
 import { createToolCallingAgent } from 'langchain/agents';
 import { ChatPromptTemplate } from '@langchain/core/prompts';
 import { AgentExecutor } from 'langchain/agents';
+import { GoogleCustomSearch } from '@langchain/community/tools/google_custom_search';
+import { HumanMessage, AIMessage } from '@langchain/core/messages';
 
 @Injectable()
 export class ChatService {
   async create(createChatDto: CreateChatDto) {
     const { query } = createChatDto;
 
-    const llm = new ChatOpenAI({
+    const model = new ChatOpenAI({
       model: 'gpt-3.5-turbo-0125',
       temperature: 0,
     });
 
-    const tools = [new TavilySearchResults({ maxResults: 1 })];
+    const googleSearchTool = new GoogleCustomSearch({
+      apiKey: process.env.GOOGLE_API_KEY,
+      googleCSEId: process.env.GOOGLE_CSE_ID,
+    });
+    googleSearchTool.name = 'google_search';
+    googleSearchTool.description =
+      'useful for when you need to perform web search to answer questions';
+
+    const tools = [googleSearchTool];
+
+    const conversationHistory = [];
 
     const prompt = ChatPromptTemplate.fromMessages([
-      ['system', 'You are a helpful assistant'],
+      [
+        'system',
+        'You are a helpful question answering assistant. Your answers sholuld be concise and accurate to avoid any confusion. Avoid providing fabricated information if uncertaion; simply acknowledge the lack of knowledge. ',
+      ],
       ['placeholder', '{chat_history}'],
       ['human', '{input}'],
       ['placeholder', '{agent_scratchpad}'],
     ]);
 
     const agent = await createToolCallingAgent({
-      llm,
+      llm: model,
       tools,
       prompt,
     });
@@ -38,10 +52,14 @@ export class ChatService {
 
     const result = await agentExecutor.invoke({
       input: query,
+      chat_history: conversationHistory,
     });
 
+    conversationHistory.push(new HumanMessage(query));
+    conversationHistory.push(new AIMessage(result.output));
+
     return {
-      result,
+      response: result.output,
     };
   }
 }
