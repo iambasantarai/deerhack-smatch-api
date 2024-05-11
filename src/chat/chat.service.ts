f78a2b616353c45b71c314e63da42c74d323d3eb
iambasantarai <iambasantarai@gmail.com>
2024-05-11 10:30:57 +0545

    Update agent that queries in db

--- src/chat/chat.service.ts
+++ src/chat/chat.service.ts
@@ -8,7 +8,7 @@ import {
   MessagesPlaceholder,
 } from '@langchain/core/prompts';
 import { AgentExecutor } from 'langchain/agents';
-import { HumanMessage, AIMessage } from '@langchain/core/messages';
+import { AIMessage } from '@langchain/core/messages';
 import { DataSource } from 'typeorm';
 import { SqlDatabase } from 'langchain/sql_db';
 import { dbCredentials } from 'src/utils/env.util';
@@ -16,9 +16,7 @@ import { SqlToolkit } from 'langchain/agents/toolkits/sql';
 
 @Injectable()
 export class ChatService {
-  async create(companyId: string, createChatDto: CreateChatDto) {
-    const { query } = createChatDto;
-
+  async getQueryResponse(companyId: string, query: string) {
     const datasource = new DataSource({
       type: 'postgres',
       host: dbCredentials.host,
@@ -30,7 +28,7 @@ export class ChatService {
 
     const db = await SqlDatabase.fromDataSourceParams({
       appDataSource: datasource,
-      includesTables: ['Company'],
+      includesTables: ['company'],
     });
 
     const model = new ChatOpenAI({
@@ -42,35 +40,37 @@ export class ChatService {
 
     const tools = sqlToolKit.getTools();
 
-    const conversationHistory = [];
-
     const SQL_PREFIX = `You are an agent designed to interact with a SQL database.
-Given an input question, create a syntactically correct {dialect} query to run, then look at the results of the query and return the answer.
-You can order the results by a relevant column to return the most interesting examples in the database.
-Query for all the columns from a specific table for most relevant answers.
-You have access to tools for interacting with the database.
-Only use the given tools. Only use the information returned by the tools to construct your final answer.
-You MUST double check your query before executing it. If you get an error while executing a query, rewrite the query and try again.
+    Given an input question, create a syntactically correct {dialect} query to fetch information from the row with id {companyId}, then look at the results of the query and return the answer.
+    You can order the results to return the most informative data in the database.
+    You have access to tools for interacting with the database.
+    Only use the given tools. Only use the information returned by the tools to construct your final answer.
+    You MUST double check your query before executing it. If you get an error while executing a query, rewrite the query and try again.
 
-DO NOT make any DML statements (INSERT, UPDATE, DELETE, DROP etc.) to the database.
+    DO NOT make any DML statements (INSERT, UPDATE, DELETE, DROP etc.) to the database.
 
-If the question does not seem related to the database table, just return "Sorry. I don't have enough knowledge to help you." as the answer.`;
+    If the question does not seem related to the database table, just return "Sorry. I don't have enough knowledge to help you." as the answer.
+
+    If the final answer is like 'The company with ID x' where x is id, just curate this answer by
+    EXCLUDING the sensetive information like ID, and unnecessary information like logo from the final answer.
+    `;
 
     const SQL_SUFFIX = `Begin!
 
-Question: {input}
-Thought: I should look for {companyId} at the table in the database to answer the question.
-{agent_scratchpad}`;
+    Question: {input}
+    Thought: I should look at the table in the database to answer the question.
+    {agent_scratchpad}`;
 
     const prompt = ChatPromptTemplate.fromMessages([
       ['system', SQL_PREFIX],
-      HumanMessagePromptTemplate.fromTemplate('{input}, {companyId}'),
+      HumanMessagePromptTemplate.fromTemplate('{input}'),
       new AIMessage(SQL_SUFFIX.replace('{agent_scratchpad}', '')),
       new MessagesPlaceholder('agent_scratchpad'),
     ]);
 
     const newPrompt = await prompt.partial({
       dialect: sqlToolKit.dialect,
+      companyId,
     });
 
     const runnableAgent = await createOpenAIToolsAgent({
@@ -86,23 +86,18 @@ Thought: I should look for {companyId} at the table in the database to answer th
 
     const result = await agentExecutor.invoke({
       input: query,
-      companyId,
-      chat_history: conversationHistory,
     });
 
-    console.log('::: RESULT :::');
-    console.log(result);
-    console.log('::: RESULT :::');
+    return result.output;
+  }
 
-    conversationHistory.push(new HumanMessage(query));
-    conversationHistory.push(new AIMessage(result.output));
+  async create(companyId: string, createChatDto: CreateChatDto) {
+    const { query } = createChatDto;
 
-    console.log('::: HISTORY :::');
-    console.log(conversationHistory);
-    console.log('::: HISTORY :::');
+    const response = await this.getQueryResponse(companyId, query);
 
     return {
-      response: result.output,
+      response,
     };
   }
 }
